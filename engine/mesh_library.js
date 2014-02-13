3da7afdbdb82d513dbaaabf2b2d2d8ee3cc8b748
Reddogstone <thereddog@gmx.net>
2014-02-13 20:51:57 +0100

    + Cylinder mesh - Quad batch (moved to outside, because it's not part of the engine, the RenderBatch is enough)

--- engine/mesh_library.js
+++ engine/mesh_library.js
@@ -16,8 +16,49 @@ var Jabaku = (function(module) {
 		};
 	}
 
+	function createCylinderData(sides) {
+		var vertices = new Array(2 * 8 * (sides + 1));
+
+		var off = 0;
+		for (var i = 0; i < sides + 1; ++i) {
+			var angle = i * 2 * Math.PI / sides;
+			var s = Math.sin(angle);
+			var c = Math.cos(angle);
+
+			vertices[off++] = 0.5 * c; vertices[off++] = 0.5 * s; vertices[off++] = 0.5; // top x, y, z
+			vertices[off++] = c; vertices[off++] = s; vertices[off++] = 0.0; // top nx, ny, nz
+			vertices[off++] = i / sides; vertices[off++] = 0.0; // top tu, tv
+			vertices[off++] = 0.5 * c; vertices[off++] = 0.5 * s; vertices[off++] = -0.5; // bottom x, y, z
+			vertices[off++] = c; vertices[off++] = s; vertices[off++] = 0.0; // bottom nx, ny, nz
+			vertices[off++] = i / sides; vertices[off++] = 1.0; // bottom tu, tv
+		}
+
+		var indices = new Array(3 * 2 * sides);
+		var off = 0;
+		for (var i = 0; i < sides; ++i) {
+			var indexOff = i * 2;
+			indices[off++] = indexOff; indices[off++] = indexOff + 1; indices[off++] = indexOff + 2;
+			indices[off++] = indexOff + 2; indices[off++] = indexOff + 1; indices[off++] = indexOff + 3;
+		}
+
+		return {
+			"vertices": vertices,
+			"indices": indices,
+			"description": {
+				"aPosition": { "components": 3, "type": "FLOAT", "normalized": false },
+				"aNormal": { "components": 3, "type": "FLOAT", "normalized": false },
+				"aTexCoord": { "components": 2, "type": "FLOAT", "normalized": false }
+			}
+		};
+	}	
+
 	function createQuadMesh(engine) {
 		var meshData = [createQuadData()];
+		return Mesh.loadFromJson(engine, meshData);
+	}
+
+	function createCylinderMesh(engine) {
+		var meshData = [createCylinderData()];
 		return Mesh.loadFromJson(engine, meshData);
 	}
 
@@ -78,5 +119,7 @@ var Jabaku = (function(module) {
 	module.createQuadData = createQuadData;
 	module.createQuadMesh = createQuadMesh;
 	module.createCubeMesh = createCubeMesh;
+	module.createCylinderData = createCylinderData;
+	module.createCylinderMesh = createCylinderMesh;
 	return module;
 })(Jabaku || {});
